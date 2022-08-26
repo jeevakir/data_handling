d92940117883e6501c1bc71c6f581bdaaedd99c2
Jeevanand Palanisamy <jeevanand0013@gmail.com>
2022-08-26 12:48:58 +0530

    Add files via upload

---
+++ ARCPY/arcgis-idw_sund.py
@@ -0,0 +1,54 @@
+import arcpy,os,time,glob
+from arcpy import env
+from arcpy.sa import *
+
+# Set environment settings
+out_gdb = r"C:\Users\JEEVA\Documents\ArcGIS\Default2.gdb"
+env.workspace = "D:\\CROP_SUMMARY_SUND\\"
+env.mask = "D:\\CROP_SUMMARY_SUND\\sundarban_landuse\\sundarban_landuse.shp"
+env.extent= "D:\\CROP_SUMMARY_SUND\\sundarban_landuse\\sundarban_landuse.shp"
+arcpy.env.parallelProcessingFactor = "100%"
+arcpy.env.overwriteOutput = True
+
+# Set local variables
+outPath = "D:\\CROP_SUMMARY_SUND\\yield-out\\"
+inPath="D:\\CROP_SUMMARY_SUND\\yield_in\\"
+mxd=arcpy.mapping.MapDocument(r"D:\\CROP_SUMMARY_SUND\\idw-yield-sund.mxd")
+symbologyLyr = r"D:\\GIS\\symbology-temp.lyr"
+df = arcpy.mapping.ListDataFrames(mxd, "*")[0]
+for file in os.listdir(inPath):
+     print file
+     infile=inPath+file
+     out_table=os.path.splitext(file)[0]
+     print out_table
+     in_table=out_gdb+"/"+out_table
+     out_layer=out_table
+     if file.endswith(".csv"):
+        print "csv-detected"
+        #arcpy.management.Delete(out_table)
+        arcpy.TableToTable_conversion(infile,out_gdb, out_table)
+        arcpy.mapping.TableView(out_gdb + "/" + out_table)
+        #arcpy.mapping.AddTableView(df, out_table)
+        arcpy.RefreshActiveView()
+        arcpy.MakeXYEventLayer_management(in_table,"LON","LAT",out_table)
+        arcpy.SaveToLayerFile_management(out_table, out_layer)
+        input = out_table
+        cellSize = 0.0017078076
+        power = 2
+        searchRadius = RadiusVariable(12, 100000)
+        arcpy.CheckOutExtension("Spatial")
+        # getting the name of fields 
+        zfield = [f.name for f in arcpy.ListFields(input)]
+        #printing the zfield for selection of necessary fields 
+        print zfield
+        # removing the latitude longitude field by defining size of field 
+        # here in this case i have removed first 2 field
+        zfield = zfield[3::]
+        #loop for selecting the z value 
+        for f in zfield:
+            print f
+            outfile = outPath + f
+            outIDW = Idw(input, f, cellSize, power, searchRadius)
+            outIDW.save( outfile + ".tif")        # change the /path/ to the folder where output to be saved 
+            #toplyr=arcpy.mapping.ListLayers(mxd,outfile)
+            #arcpy.management.ApplySymbologyFromLayer(toplyr, symbologyLyr)    
---
+++ ARCPY/idw-out-maps_sund.py
@@ -0,0 +1,93 @@
+import arcpy,os,glob,time
+from arcpy import env
+from arcpy.sa import *
+arcpy.env.parallelProcessingFactor = "100%"
+arcpy.env.overwriteOutput = True
+
+incrop=["GREENGRAM","RICE","LATHYRUS"]
+intime=["BASE","NEAR","SHORT","MID"]
+inyear=[" ","2022-2030","2041-2050","2051-2060"]
+inscen=["245","585"]
+# Check out the ArcGIS Spatial Analyst extension license
+arcpy.CheckOutExtension("Spatial")
+inPath = "D:\\CROP_SUMMARY_SUND\\yield_out\\"
+env.workspace = r"D:\\CROP_SUMMARY_SUND\\"
+outPath=r"D:\\CROP_SUMMARY_SUND\\maps_out\\"
+#symbologyLyr = r"D:\\CROP_SUMMARY_SUND\\symbology-temp.lyr"
+
+
+for filename in glob.glob(inPath+'*Shape.tif'):
+    os.remove(filename)
+for y in inscen:
+    for x in incrop:
+        mxd1 = arcpy.mapping.MapDocument(r"D:\\CROP_SUMMARY_SUND\\yield_sund_maps_temp.mxd")
+        path=inPath +x+"*"+y+"*.tif"
+        mxd1.saveACopy(outPath+x+"_"+y+".mxd")
+        mxd=arcpy.mapping.MapDocument(outPath+x+"_"+y+".mxd")
+        for file in glob.glob(path):
+            if not "DEV" in file:
+                symbologyLyr =r"D:\\CROP_SUMMARY_SUND\\symb\\symbology_"+x+".lyr"
+                dnnm=file.split('\\')
+                #print (dnnm[3])
+                dnnme=dnnm[3].split('_')
+                dnindex=intime.index(dnnme[1])
+                #print(str(dnnme[1]) + "__"+str(dnindex))
+                df=arcpy.mapping.ListDataFrames(mxd)[dnindex]
+                addLayer = arcpy.mapping.Layer(file)
+                arcpy.mapping.AddLayer(df,addLayer,"BOTTOM")
+                #print ("FILE= "+file)
+                symlayer=arcpy.mapping.Layer(symbologyLyr)
+                uplayer=arcpy.mapping.ListLayers(mxd, addLayer.name , df)[0]
+                arcpy.mapping.UpdateLayer(df,uplayer,symlayer,True)
+                for elm in arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT","Text"+str(dnindex)):
+                    elm.text = str(dnnme[1]) + " ("+ str(inyear[dnindex])+")"
+                       
+     
+        arcpy.RefreshActiveView()
+        Dtitle=arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT","TITLEtext")[0]
+        lowx=x.lower()
+        Dtitle.text = "Yield of "+str(lowx)+ " under SSP"+y
+        mxd.save()
+        arcpy.mapping.ExportToPNG(mxd,outPath+x+"_"+y+".png")
+        #time.sleep(10)
+
+
+
+
+
+
+
+
+
+#for x in intiff:
+#    mxd = arcpy.mapping.MapDocument(r"D:\\CROP_SUMMARY_SUND\\"+x+"yield_maps.mxd")
+#    path=inPath +x+"*.tif"
+#    print path
+#    dn=0
+#    if "DEV" in x:
+#        mxd = arcpy.mapping.MapDocument(r"D:\\CROP_SUMMARY_SUND\\"+x+"yield_dev_maps.mxd")
+#for file in glob.glob(inPath+"*.tif"):  
+#    fname=file.split('.')
+#    #mxd1.saveACopy(outPath+fname+".mxd")
+#    #mxd=arcpy.mapping.MapDocument(outPath+fname+".mxd")
+#    for file in glob.glob(path):
+#        df=arcpy.mapping.ListDataFrames(mxd)[dn]
+#        addLayer = arcpy.mapping.Layer(file)
+#        yrf=addLayer.name.split('_F')
+#        titl=yrf[1].split('.')
+#        arcpy.mapping.AddLayer(df,addLayer,"BOTTOM")
+#        symlayer=arcpy.mapping.Layer(symbologyLyr)
+#        uplayer=arcpy.mapping.ListLayers(mxd, addLayer.name , df)[0]
+#        arcpy.mapping.UpdateLayer(df,uplayer,symlayer,True)
+#        df.name = titl[0]
+#        for elm in arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT","Text"+str(dn)):
+#            print elm.text
+#            elm.text = str(titl[0])
+#            print elm.text
+#        dn=dn+1
+#    arcpy.RefreshActiveView()
+#    mxd.save()
+#    arcpy.mapping.ExportToPNG(mxd,outPath+x+".png")
+#        
+            
+    
